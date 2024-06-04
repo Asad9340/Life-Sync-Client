0d86836ed745f5640365096eeeffa5380abc27df
Asad9340 <asadulimran1999@gmail.com>
2024-06-04 14:55:10 -0600

    chagnes on the design

--- src/Dashboard/Board/AdminAnalysis.jsx
+++ src/Dashboard/Board/AdminAnalysis.jsx
@@ -1,6 +1,7 @@
 import { BiSolidDonateBlood } from 'react-icons/bi';
 import { RiRefund2Line } from 'react-icons/ri';
 import { MdBloodtype } from 'react-icons/md';
+
 const AdminAnalysis = () => {
   return (
     <div className="container mx-auto px-4 py-8">
@@ -8,26 +9,26 @@ const AdminAnalysis = () => {
         Welcome to Admin Data Analytics
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-white shadow-md p-6 rounded-lg">
+        <div className="bg-blue-100 shadow-md p-6 rounded-lg">
           <div className="flex justify-center">
-            <BiSolidDonateBlood className="text-8xl" />
+            <BiSolidDonateBlood className="text-8xl text-blue-500" />
           </div>
           <h2 className="text-xl font-semibold text-center">Total Donors</h2>
-          <p className="text-center">$1,500,000</p>
+          <p className="text-center text-blue-700">$1,500,000</p>
         </div>
-        <div className="bg-white shadow-md p-6 rounded-lg">
+        <div className="bg-green-100 shadow-md p-6 rounded-lg">
           <div className="flex justify-center">
-            <RiRefund2Line className="text-8xl" />
+            <RiRefund2Line className="text-8xl text-green-500" />
           </div>
-          <h2 className="text-xl font-semibold text-center">Website Traffic</h2>
-          <p className="text-center">50,000</p>
+          <h2 className="text-xl font-semibold text-center">Total Funding</h2>
+          <p className="text-center text-green-700">50,000</p>
         </div>
-        <div className="bg-white shadow-md p-6 rounded-lg">
+        <div className="bg-yellow-100 shadow-md p-6 rounded-lg">
           <div className="flex justify-center">
-            <MdBloodtype className="text-8xl" />
+            <MdBloodtype className="text-8xl text-yellow-500" />
           </div>
-          <h2 className="text-xl font-semibold text-center">User Engagement</h2>
-          <p className="text-center">10,000</p>
+          <h2 className="text-xl font-semibold text-center">Total Blood Donation Request</h2>
+          <p className="text-center text-yellow-700">10,000</p>
         </div>
       </div>
     </div>
