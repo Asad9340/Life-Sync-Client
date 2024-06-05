9aa008414a846fa3b10724896a398b1fa5d6a851
Asad9340 <asadulimran1999@gmail.com>
2024-06-05 03:00:57 -0600

    in all user page for admin can now update user status make volunteer , make admin ,block user

--- src/Dashboard/AllUsers/AllUsers.jsx
+++ src/Dashboard/AllUsers/AllUsers.jsx
@@ -1,20 +1,76 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Firebase/AuthProvider';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 function AllUsers() {
   const [userData, setUserData] = useState([]);
   const { user } = useContext(AuthContext);
+  const [control, setControl] = useState(false);
   useEffect(() => {
     (async () => {
       const { data } = await axios.get(`http://localhost:5000/users`);
-      console.log(data[0]?.role);
       setUserData(data);
     })();
-  }, [user?.email]);
-  console.log(userData);
+  }, [user?.email, control]);
+  const handleBlock = async (id, status, role) => {
+    if (status == 'active' && role !== 'admin') {
+      const response = await axios.patch(
+        `http://localhost:5000/users/block/${id}`
+      );
+      if (response.data.modifiedCount) {
+        Swal.fire('Successful updated Status');
+        setControl(!control);
+      }
+    } else if (role === 'admin') {
+      Swal.fire('You can not block Admin');
+    } else {
+      Swal.fire('Status is already Blocked');
+    }
+  };
+  const handleUnBlock = async (id, status, role) => {
+    if (status == 'block' && role !== 'admin') {
+      const response = await axios.patch(
+        `http://localhost:5000/users/active/${id}`
+      );
+      if (response.data.modifiedCount) {
+        Swal.fire('Successful updated Status');
+        setControl(!control);
+      }
+    } else if (role === 'admin') {
+      Swal.fire('You can not Unblock Admin');
+    } else {
+      Swal.fire('Status is already active');
+    }
+  };
+  const handleVolunteer = async (id, role) => {
+    if (role == 'donor') {
+      const response = await axios.patch(
+        `http://localhost:5000/users/volunteer/${id}`
+      );
+      if (response.data.modifiedCount) {
+        Swal.fire('Successful updated Status');
+        setControl(!control);
+      }
+    } else {
+      Swal.fire('Account is already Volunteer');
+    }
+  };
+  const handleAdmin = async (id, role) => {
+    if (role == 'donor' || role =='volunteer') {
+      const response = await axios.patch(
+        `http://localhost:5000/users/makeAdmin/${id}`
+      );
+      if (response.data.modifiedCount) {
+        Swal.fire('Successful updated Status');
+        setControl(!control);
+      }
+    } else {
+      Swal.fire('Account is already Admin');
+    }
+  };
   return (
-    <div className='my-10'>
+    <div className="my-10">
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -46,22 +102,42 @@ function AllUsers() {
                 <td>{item?.role}</td>
                 <td>{item?.status}</td>
                 <td>
-                  <button className="btn btn-sm btn-active btn-error">
+                  <button
+                    onClick={() =>
+                      handleBlock(item?._id, item?.status, item?.role)
+                    }
+                    className="btn btn-sm btn-active btn-error"
+                  >
                     Block
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-sm btn-active btn-primary">
+                  <button
+                    onClick={() =>
+                      handleUnBlock(item?._id, item?.status, item?.role)
+                    }
+                    className="btn btn-sm btn-active btn-primary"
+                  >
                     Unblock
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-sm btn-active btn-primary">
+                  <button
+                    onClick={() =>
+                      handleVolunteer(item?._id, item?.role)
+                    }
+                    className="btn btn-sm btn-active btn-primary"
+                  >
                     Volunteer
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-sm btn-active btn-primary">
+                  <button
+                    onClick={() =>
+                      handleAdmin(item?._id, item?.role)
+                    }
+                    className="btn btn-sm btn-active btn-primary"
+                  >
                     Admin
                   </button>
                 </td>
